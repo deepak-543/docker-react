771858a67780fdb31ea5fad589204043b4c623f8
DeepakVerma <deepakverma.dv0598@gmail.com>
2021-01-23 17:51:36 +0530

    Commit

--- src/App.test.js
+++ src/App.test.js
@@ -12,3 +12,11 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/);
+  expect(linkElement).toBeInTheDocument();
+});
+
